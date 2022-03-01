0131f1e37639f38000f49cebb6c519809f10cf59
Achilles Benetopoulos <abenetopoulos@gmail.com>
2022-02-28 20:08:56 -0800

    Stupid language

--- db/cache/cache.h
+++ db/cache/cache.h
@@ -16,9 +16,9 @@ using namespace std;
 namespace ROCKSDB_NAMESPACE {
   // TODO consider making this a template
   struct eviction_policy {
-    virtual void MarkInsertion(string& key);
-    virtual void MarkAccess(string& key);
-    virtual string Evict();
+    virtual void MarkInsertion(string& key) = 0;
+    virtual void MarkAccess(string& key) = 0;
+    virtual string Evict() = 0;
 
     virtual ~eviction_policy() { }
   };
